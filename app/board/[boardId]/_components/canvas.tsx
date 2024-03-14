ebbe04d5393bf1110278460a6a7e7fcf8b9ad466
llt <418797644@qq.com>
2024-03-14 23:09:54 +0800

    update

--- app/board/[boardId]/_components/canvas.tsx
+++ app/board/[boardId]/_components/canvas.tsx
@@ -4,13 +4,30 @@ import React from 'react'
 import Info from './info'
 import Participants from './participants'
 import Toolbar from './toolbar'
+import { CanvasMode, CanvasState } from '@/types/canvas'
+import { useCanRedo, useCanUndo, useHistory } from '@/liveblocks.config'
 
 export default function Canvas({ boardId }: { boardId: string }) {
+  const [canvasState, setCanvasState] = React.useState<CanvasState>({
+    mode: CanvasMode.None,
+  })
+
+  const history = useHistory()
+  const canUndo = useCanUndo()
+  const canRedo = useCanRedo()
+
   return (
     <main className="min-h-screen relative bg-neutral-100 touch-none">
       <Info boardId={boardId} />
       <Participants />
-      <Toolbar />
+      <Toolbar
+        canvasState={canvasState}
+        setCanvasState={setCanvasState}
+        canRedo={canRedo}
+        canUndo={canUndo}
+        undo={history.undo}
+        redo={history.redo}
+      />
     </main>
   )
 }
